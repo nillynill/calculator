c9aab84983996ee013b9bbaf4a62362d27eabc9e
nillynill <114229285+nillynill@users.noreply.github.com>
2025-07-18 11:47:30 -0700

    Code functions for basic arithmetic operations

---
+++ calculator.js
@@ -0,0 +1,38 @@
+
+
+function add(number1,number2) {
+    return result = number1 + number2;
+}
+add();
+console.log(add(3,5));
+
+
+
+function subtract(number1,number2) {
+    return result = number1 - number2;
+}
+subtract();
+console.log(subtract(3,5));
+
+
+
+function multiply(number1,number2) {
+    return result = number1 * number2;
+}
+multiply();
+console.log(multiply(3,5));
+
+
+
+function divide(number1,number2) {
+    return result = number1 / number2;
+}
+divide();
+console.log(divide(3,5));
+
+
+
+/*function operate(operator, number1, number2){
+    return add();
+}*/
+
